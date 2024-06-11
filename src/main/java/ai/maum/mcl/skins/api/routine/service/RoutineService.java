05e79d53fef7d684dc706512381af03cb7408c98
Jeremy <ryujeongmo>
2024-06-11 15:46:29 +0900

    update service check

---
+++ src/main/java/ai/maum/mcl/skins/api/routine/service/RoutineService.java
@@ -0,0 +1,37 @@
+package ai.maum.mcl.skins.api.routine.service;
+
+import ai.maum.mcl.skins.api.member.mapper.MemberMapper;
+import ai.maum.mcl.skins.api.member.model.MemberList;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class RoutineService {
+    private final MemberMapper memberMapper;
+
+    @Scheduled(fixedRate = 1 * 60 * 1000)
+    public void executeRoutine() {
+        log.info("-------------- ROUTINE START ---------------------");
+        Map<String, Object> param = new HashMap<>();
+        List<MemberList> allMembers = memberMapper.findListMemberById(param);
+        
+        List<Long> idList = new ArrayList<Long>();
+        
+
+    }
+}
