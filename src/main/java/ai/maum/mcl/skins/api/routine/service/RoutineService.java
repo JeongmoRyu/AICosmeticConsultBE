87d9d2be93fb1481b749c2b4de777bf7499ee952
Jeremy <ryujeongmo>
2024-06-11 16:57:56 +0900

    update

--- src/main/java/ai/maum/mcl/skins/api/routine/service/RoutineService.java
+++ src/main/java/ai/maum/mcl/skins/api/routine/service/RoutineService.java
@@ -1,6 +1,7 @@
 package ai.maum.mcl.skins.api.routine.service;
 
 import ai.maum.mcl.skins.api.member.mapper.MemberMapper;
+import ai.maum.mcl.skins.api.member.model.MemberChatTime;
 import ai.maum.mcl.skins.api.member.model.MemberList;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -13,6 +14,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,23 +28,37 @@ import java.util.Map;
 public class RoutineService {
     private final MemberMapper memberMapper;
 
-    @Scheduled(fixedRate = 1 * 60 * 1000)
-    public void executeRoutine() {
+    @Scheduled(fixedRate = 1*60*1000)
+    public void executeRoutine() throws IOException {
         log.info("-------------- ROUTINE START ---------------------");
-        Map<String, Object> param = new HashMap<>();
-        List<MemberList> allMembers = memberMapper.findListMemberById(param);
-        
+        List<MemberChatTime> allMembers = memberMapper.findMemberWithChatUpdated();
+        log.info("allmembers: {}",allMembers);
         List<Long> idList = new ArrayList<>();
-        LocalDateTime now = LocalDateTime.now();
+        Timestamp now = Timestamp.from(Instant.now());
+        Timestamp twentyFourHoursAgo = Timestamp.from(Instant.now().minusSeconds(24*60*60));
 
-        for (MemberList member : allMembers) {
-            if (member.getChatUpdated() != null && member.getChatUpdated().isBefore(now.minusHours(24))) {
-                idList.add(member.getId());
+        for (MemberChatTime member : allMembers) {
+            Timestamp chatUpdated = member.getChatUpdated();
+            if (chatUpdated != null && chatUpdated.after(twentyFourHoursAgo)) {
+                Long selectedId = member.getId();
+                idList.add(selectedId);
+                List<ChatroomDetail> chatroomDetailList = new ArrayList<ChatroomDetail>();
+                String strData = callProaiService.callProaiGet("/extapi/inner/chatroom/detail/" + selectedId);
+                log.info("strData :{}",strData);
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode data = objectMapper.readTree(strData).path("data");
+                chatroomDetailList = objectMapper.readValue(
+                        data.traverse(),
+                        new TypeReference<List<ChatroomDetail>>() {});
+
             }
         }
 
-        log.info("IDs with chat_updated older than 24 hours: {}", idList);
+        log.info("IDs with chat_updated less than 24 hours: {}", idList);
+        for (Long id: idList) {
+
+        }
         log.info("-------------- ROUTINE END ---------------------");
     }
-}
 
+}
