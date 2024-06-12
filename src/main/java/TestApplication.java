f78c7787abe850716499b42aad8c63eb22397206
Jeremy <ryujeongmo>
2024-06-12 17:00:40 +0900

    update

--- src/main/java/TestApplication.java
+++ src/main/java/TestApplication.java
@@ -15,3 +15,36 @@ public class TestApplication {
 //        System.out.println("decryptText:" + decryptText);
     }
 }
+
+// 아래와 같은 rag.proto로 되어 있는 python 서버가 있는데,
+// springboot가 client가 되어서 python 서버와 streaming 통신을 하려고 해.
+
+// 기본적인 spring 코드를 작성해줘
+
+// syntax = "proto3";
+
+// package rag_service.rag_module;
+
+// service RagService {
+// rpc ChatHandler (stream ChatRequest) returns (stream ChatResponse);
+// };
+
+// message Chat {
+// string input = 1;
+// string output = 2;
+// }
+
+// message ChatResponse {
+// oneof msg_type {
+// string msg = 1;
+// Status status = 2;
+// }
+// }
+
+// message ChatRequest {
+// oneof msg_type {
+// Config config = 1;
+// string msg = 2;
+// }
+// optional int64 sequence = 3;
+// }
